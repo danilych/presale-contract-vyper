1ed36878bb9a408ba68940ce817e5964c3b8b73c
danilych <danilych45@proton.me>
2025-01-04 20:19:02 +0100

    test(deposit_tokens): it reverts when tokens transfer failed

--- test/concrete/deposit_tokens/Presale.depositTokens.t.sol
+++ test/concrete/deposit_tokens/Presale.depositTokens.t.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import { PresaleTest } from "../../PresaleTest.sol";
-import { IPresale } from "../../interfaces/IPresale.sol";
+import {PresaleTest} from "../../PresaleTest.sol";
+import {IPresale} from "../../interfaces/IPresale.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract PresaleDepositTokensTest is PresaleTest {
+contract PresaledepositTokens is PresaleTest {
     function setUp() public {
         vm.startPrank(deployer);
         fixture();
@@ -36,27 +37,37 @@ contract PresaleDepositTokensTest is PresaleTest {
 
     function test_WhenOwnerDepositTokens() external {
         uint256 amount = 100 ether;
-        
+
         // Setup token approval
         vm.startPrank(deployer);
         token.approve(address(presale), amount);
-        
+
         // it transfers tokens
         uint256 balanceBefore = token.balanceOf(address(presale));
 
         // it emits
         vm.expectEmit(true, true, true, true);
         emit IPresale.TokensDeposited(deployer, amount);
-        
+
         presale.deposit_tokens(amount);
         uint256 balanceAfter = token.balanceOf(address(presale));
-        
+
         // it updates balance
         assertEq(balanceAfter - balanceBefore, amount, "Token balance not updated correctly");
-        
+
         // it state is updated - tokens are transferred from owner to presale
         assertEq(token.balanceOf(address(presale)), amount, "Presale balance not updated");
-        
+
         vm.stopPrank();
+    }
+
+    function test_WhenTokenTransferIsNotSuccessful() external {
+        // it reverts
+        uint256 amount = 100 ether;
+        
+        // Try to deposit without approval
+        vm.prank(deployer);
+        vm.expectRevert(abi.encodeWithSignature("ERC20InsufficientAllowance(address,uint256,uint256)", address(presale), 0, amount));
+        presale.deposit_tokens(amount);
     }
 }
