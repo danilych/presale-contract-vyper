25436eb2d89494aab7907a161e08f0486b631b87
danilych <danilych45@proton.me>
2025-01-04 20:03:40 +0100

    chore(deposit_tokens): scaffold tests

---
+++ test/concrete/deposit_tokens/Presale.depositTokens.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+
+contract PresaledepositTokens {
+    function test_WhenSaleIsAlreadyStarted() external {
+        // it reverts
+    }
+
+    function test_WhenNotOwnerTryToDepositTokens() external {
+        // it reverts
+    }
+
+    function test_WhenAmountIsZero() external {
+        // it reverts
+    }
+
+    function test_WhenOwnerDepositTokens() external {
+        // it emits
+        // it transfers tokens
+        // it updates balance
+        // it state is updated
+    }
+}
